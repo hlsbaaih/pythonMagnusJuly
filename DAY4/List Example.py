99f63b66f74ffa929e8ebdb2d8ff2db102249e2b
hlsbaaih <simmy.2026@gmail.com>
2023-08-01 01:05:19 -0700

    day4_26_july_2023

---
+++ DAY4/List Example.py
@@ -0,0 +1,24 @@
+a1=[10,30,23,'abc',10.5,'hello']
+
+print(a1)
+print("welcome to python")
+print(a1[3])
+print(a1[0:3])
+print(a1[3:6])
+print(a1[2:])
+print(a1[:5])
+print(a1)
+a1.append(120)
+print(a1)
+a1.insert(3,'hi')
+print(a1)
+a1[6]="welcome"
+print(a1)
+a1.remove('abc')
+print(a1)
+a1.pop()
+print(a1)
+a1.pop(4)
+print(a1)
+
+
