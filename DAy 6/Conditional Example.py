170c4f229bd58f5ae9389d7df23d15900a0cd4a5
hlsbaaih <simmy.2026@gmail.com>
2023-08-01 16:19:27 -0700

    Day6_28_July_2023

---
+++ DAy 6/Conditional Example.py
@@ -0,0 +1,21 @@
+a=30
+b=20
+if a>b:
+    print("Greater Value")
+a=10
+b=20
+if a>b:
+    print("Greater Value")
+else:
+    print("Lesser Value")
+a=10
+b=20
+if a>b:
+    print("Greater Value")
+elif a<b:
+    print("Lesser Value")
+elif a==b:
+    print("Equals")
+else:
+    print("Not Valid")
+
